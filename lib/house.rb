dfc735cba3b9f36b050110f46967bc9581c9c827
dandyandy22 <andrasm96@gmail.com>
2022-04-21 09:40:44 -0700

    Add line two of song

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,9 @@
 class House
   def line(number)
-    "This is the house that Jack built.\n"
+    if number == 1
+      "This is the house that Jack built.\n"
+    elsif number == 2
+      "This is the malt that lay in the house that Jack built.\n"
+    end
   end
 end
