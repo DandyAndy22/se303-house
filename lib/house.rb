d85b398ef26f8961fa1b6fe1523ed31fdc3c192a
dandyandy22 <andrasm96@gmail.com>
2022-05-25 13:43:55 -0700

    Line: Extract text unique to line 11

--- lib/house.rb
+++ lib/house.rb
@@ -19,16 +19,26 @@ class House
     elsif number == 9
       "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     elsif number == 10
-      "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    elsif number == 11
-      "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    elsif number == 12
-      "This is #{the_horse()}the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is #{the_farmer(number)}the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    else
+      "This is #{the_horse(number)}#{the_farmer(number)}the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 
-  def the_horse()
+  def the_horse(number)
+    if number == 12
     "the horse and the hound and the horn that belonged to "
+    elsif number == 11
+      ""
+    end
+  end
+
+  def the_farmer(number)
+    if number == 10
+      ""
+    else
+      "the farmer sowing his corn that kept "
+    end
   end
 
 
